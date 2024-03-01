c40e7f7e9665ea87eada15eeb499aa64f32b3b13
Daniel Adam Coats <coats.daniel@sosasou.cz>
2024-03-01 11:35:38 +0100

    more work

--- widgets/admin/user_manager.php
+++ widgets/admin/user_manager.php
@@ -1,23 +1,16 @@
 <?php
 namespace UserManager;
 
+require "../utils/user.php"
+
 function Render() 
 {
-    ?>
-    <form class="inline_form" action="admin/add_product.php" method="POST">
-        <label>Title: </label>
-        <input type="text" name="title" required>
-        <label>Spec: </label>
-        <input type="text" name="spec" required>
-        <label>Resources: </label>
-        <input type="text" name="resources" required>
-        <label>Price: </label>
-        <input value="100" type="number" name="price" required>
-        <label>Stock: </label>
-        <input type="number" value="1", name="stock" required>
-        <label>Category: </label>
-        <input type="text", name="category" required>
-        <button type="submit">Submit</button>
-    </form>
-    <?php
+    $users = \User\GetAllAccounts(); 
+    
+    while ($user = $users->fetch_assoc();) 
+    {
+        echo "<form>";
+        
+        echo "</form>"
+    }
 }
